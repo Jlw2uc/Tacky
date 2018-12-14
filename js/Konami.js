6a3fb56a943d271d9a84ce66e3b39c2a1213f86d
Jessica Wilson <jessica.wilson@maxxpotential.com>
2018-12-14 13:49:07 -0500

    version 2.2

--- js/Konami.js
+++ js/Konami.js
@@ -7,8 +7,6 @@ $(document).keydown(function(e) {
   kkeys.push( e.keyCode );
   if ( kkeys.toString().indexOf( konami ) >= 0 ){
     $(document).unbind('keydown',arguments.callee);
-
-    $('main').css('background-color', 'red');
     $('#snowflake').css('display', 'block');
     alert("Let it snow!!");
   }
